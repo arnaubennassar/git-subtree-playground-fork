7093a15e155e96fc1afad9235be125fe419870c5
Arnau <arnaubennassar5@gmail.com>
2023-04-21 17:21:46 +0200

    Add hello package in the subtree from the upstream repo

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/arnaubennassar/git-subtree-playground-fork
 
 go 1.19
+
+require github.com/arnaubennassar/git-subtree-playground-upstream v0.0.0-20230421150006-83c36b03e7fc
--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/arnaubennassar/git-subtree-playground-fork/hello"
+)
 
 func main() {
 	fmt.Println("hello from the fork repo")
+	hello.SayHello()
+	hello.SayHelloFromInternalDependency()
 }
